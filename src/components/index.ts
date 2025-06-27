3aa00aec8283a97dd558a5db819a0a22ce1be18b
colin <colinrozzi@gmail.com>
2025-06-27 12:10:49 -0400

    Fix: Remove StatusHeader export from components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@
  */
 
 export { MessageComponent } from './MessageComponent.js';
-export { StatusHeader } from './StatusHeader.js';
 export { SimpleInput } from './SimpleInput.js';
 export { MultiLineInput } from './MultiLineInput.js';
 export { SmartInput } from './SmartInput.js';
