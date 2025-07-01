a599054fde8af7d3e22dc01c17394827ad261377
colin <colinrozzi@gmail.com>
2025-07-01 09:27:30 -0400

    Remove MultiLineInputManaged from component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,5 @@
 export { MessageComponent } from './MessageComponent.js';
 export { SimpleInput } from './SimpleInput.js';
 export { MultiLineInput } from './MultiLineInput.js';
-export { MultiLineInputManaged } from './MultiLineInputManaged.js';
 export { SmartInput } from './SmartInput.js';
 export { HelpPanel } from './HelpPanel.js';
